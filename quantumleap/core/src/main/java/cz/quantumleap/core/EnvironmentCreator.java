19cc160cef84c9e7c0c3fa9c6cb19c6e24365850
Václav Kužel <vkuzel@gmail.com>
2016-04-15 22:05:09 +0200

    New version of spring-boot-multi-project that supports mupltiple main classes in project. Prevented interference of EnvironmentCreator configuration with Application configuration.

--- quantumleap/core/src/main/java/cz/quantumleap/core/EnvironmentCreator.java
+++ quantumleap/core/src/main/java/cz/quantumleap/core/EnvironmentCreator.java
@@ -1,13 +1,10 @@
 package cz.quantumleap.core;
 
 import cz.quantumleap.core.common.ResourceManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
@@ -17,16 +14,8 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.List;
 
-@Configuration
+//@Configuration
 public class EnvironmentCreator implements CommandLineRunner {
-
-    private static final Logger log = LoggerFactory.getLogger(EnvironmentCreator.class);
-
-    public static void main(String[] args) throws IOException {
-        SpringApplication application = new SpringApplication(EnvironmentCreator.class);
-        application.setWebEnvironment(false);
-        application.run(args);
-    }
 
     @Bean
     public ResourceManager resourceManager() {
@@ -46,10 +35,19 @@ public class EnvironmentCreator implements CommandLineRunner {
         );
     }
 
+    public static void main(String[] args) throws IOException {
+        SpringApplication application = new SpringApplication(EnvironmentCreator.class);
+        application.setWebEnvironment(false);
+        application.run(args);
+    }
+
     @Override
     public void run(String... args) throws Exception {
+        // TODO drush ... cz.quantumleap.server, cz.quantumleap.cli
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        // TODO Sort resources by name!
         List<Resource> scripts = resourceManager().findOnClasspath("db/scripts/*_*.sql");
+        // TODO Add support for Postgres functions and procedures.
         scripts.forEach(populator::addScript);
         populator.execute(dataSource());
     }
