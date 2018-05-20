0747e1fcc4aaabe9666bdb3de1eb2c1423a00b51
vkuzel <vkuzel@gmail.com>
2018-05-20 08:45:30 +0200

    * Improved request mapping security.
        Previous solution was based on matching a request methods and
        request mapping patterns. This was insufficient in case of
        "overlapping" patterns where additional conditions
        (like RequestCondition) were involved.
    
        Now the RequestMappingInfo.getMatchingCondition() is used to
        determine whether a request really matches particular mapping.

--- core/src/main/java/cz/quantumleap/core/security/configurer/RequestMappingAwareHttpSecurityConfigurer.java
+++ core/src/main/java/cz/quantumleap/core/security/configurer/RequestMappingAwareHttpSecurityConfigurer.java
@@ -1,27 +1,23 @@
 package cz.quantumleap.core.security.configurer;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
-import org.apache.commons.lang3.Validate;
 import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class RequestMappingAwareHttpSecurityConfigurer {
 
-    private static final String[] STATIC_CONTENT_ENDPOINTS = new String[] {
+    private static final String[] STATIC_CONTENT_ENDPOINTS = new String[]{
             "/webjars/**", "/assets/**"
     };
     private static final String SPEL_PERMIT_ALL = "permitAll()";
@@ -33,46 +29,12 @@ public class RequestMappingAwareHttpSecurityConfigurer {
     }
 
     public void configure(HttpSecurity httpSecurity) throws Exception {
-
         ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = httpSecurity.authorizeRequests();
-
         List<RequestMappingInfo> permitAllMappingInfo = findRequestMappingInfo(this::hasPermitAll);
-
-        Multimap<HttpMethod, String> patternsByMethod = groupPatternsByHttpMethod(permitAllMappingInfo);
-        patternsByMethod.asMap().forEach((method, patterns) ->
-                registry.mvcMatchers(method, patterns.toArray(new String[]{})).permitAll()
-        );
-
-        List<String> patterns = getPatternsWithoutHttpMethod(permitAllMappingInfo);
-        registry.mvcMatchers(patterns.toArray(new String[]{})).permitAll();
-
         registry
+                .requestMatchers(new MappingInfoRequestMatcher(permitAllMappingInfo)).permitAll()
                 .antMatchers(STATIC_CONTENT_ENDPOINTS).permitAll()
                 .anyRequest().authenticated();
-    }
-
-    private Multimap<HttpMethod, String> groupPatternsByHttpMethod(List<RequestMappingInfo> requestMappingInfo) {
-
-        Multimap<HttpMethod, String> patternsByMethod = ArrayListMultimap.create();
-
-        for (RequestMappingInfo mappingInfo : requestMappingInfo) {
-            Set<RequestMethod> methods = mappingInfo.getMethodsCondition().getMethods();
-
-            methods.forEach(method -> {
-                HttpMethod httpMethod = HttpMethod.resolve(method.name());
-                Validate.notNull(httpMethod, "Unknown method %s", method);
-                patternsByMethod.putAll(httpMethod, mappingInfo.getPatternsCondition().getPatterns());
-            });
-        }
-
-        return patternsByMethod;
-    }
-
-    private List<String> getPatternsWithoutHttpMethod(List<RequestMappingInfo> requestMappingInfo) {
-        return requestMappingInfo.stream()
-                .filter(rmi -> rmi.getMethodsCondition().getMethods() == null || rmi.getMethodsCondition().getMethods().isEmpty())
-                .flatMap(rmi -> rmi.getPatternsCondition().getPatterns().stream())
-                .collect(Collectors.toList());
     }
 
     private List<RequestMappingInfo> findRequestMappingInfo(Predicate<HandlerMethod> handlerMethodPredicate) {
@@ -97,5 +59,24 @@ public class RequestMappingAwareHttpSecurityConfigurer {
         String spEl = preAuthorize.value();
         spEl = spEl.replace(" ", "");
         return spEl.contains(SPEL_PERMIT_ALL);
+    }
+
+    private static class MappingInfoRequestMatcher implements RequestMatcher {
+
+        private final List<RequestMappingInfo> mappingInfo;
+
+        public MappingInfoRequestMatcher(List<RequestMappingInfo> mappingInfo) {
+            this.mappingInfo = mappingInfo;
+        }
+
+        @Override
+        public boolean matches(HttpServletRequest request) {
+            for (RequestMappingInfo requestMappingInfo : mappingInfo) {
+                if (requestMappingInfo.getMatchingCondition(request) != null) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
