f47cb4f483103a084dd03dff5a57ceaf232307fa
vkuzel <vkuzel@gmail.com>
2018-05-27 22:28:55 +0200

    * Support for per word SQL condition builder. * Regenerated domain objects.

--- core/src/main/java/cz/quantumleap/core/common/Utils.java
+++ core/src/main/java/cz/quantumleap/core/common/Utils.java
@@ -1,6 +1,9 @@
 package cz.quantumleap.core.common;
 
 import com.google.common.io.CharStreams;
+import org.jooq.Condition;
+import org.jooq.TableField;
+import org.jooq.impl.DSL;
 import org.springframework.core.io.Resource;
 import org.springframework.util.StringUtils;
 
@@ -23,6 +26,21 @@ public class Utils {
 
     public static boolean isAjaxRequest(HttpServletRequest request) {
         return AJAX_HEADER_VALUE.equals(request.getHeader(AJAX_HEADER_NAME));
+    }
+
+    public static Condition buildPerWordLikeCondition(TableField<?, String> field, String query) {
+        // TODO With jOOQ 3.11 use DSL.noCondition()
+        Condition condition = DSL.condition(true);
+        if (org.apache.commons.lang3.StringUtils.isBlank(query)) {
+            return condition;
+        }
+
+        for (String word : query.split("\\s+")) {
+            String binding = Utils.escapeLikeBinding(word, '!');
+            condition = condition.and(field.likeIgnoreCase(word + "%", '!')
+                    .or(field.likeIgnoreCase("% " + binding + "%", '!')));
+        }
+        return condition;
     }
 
     public static String escapeLikeBinding(String binding, char escapeChar) {
