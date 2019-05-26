625a17b8d52b9c91a98fb8bd01dd4c60bacb53ed
vkuzel <vkuzel@gmail.com>
2019-05-26 13:45:49 +0200

    * Added support for asymmetric cryptography to SecurityUtils.

--- core/src/main/java/cz/quantumleap/core/common/SecurityUtils.java
+++ core/src/main/java/cz/quantumleap/core/common/SecurityUtils.java
@@ -5,9 +5,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.codec.Hex;
 import org.springframework.security.crypto.encrypt.Encryptors;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
 
 public class SecurityUtils {
@@ -19,28 +25,94 @@ public class SecurityUtils {
         return passwordEncoder.encode(password);
     }
 
-    public static boolean checkEncodedPassword(String password, String encodedPassword) {
+    public static boolean verifyEncodedPassword(String password, String encodedPassword) {
         return passwordEncoder.matches(password, encodedPassword);
     }
 
-    public static byte[] encryptText(String password, String text) {
+    public static byte[] encryptMessageByPassword(String password, String message) {
         try {
             byte[] salt = new byte[PASSWORD_SALT_LENGTH_BYTES];
             SecureRandom.getInstanceStrong().nextBytes(salt);
-            byte[] encrypted = Encryptors.stronger(password, new String(Hex.encode(salt))).encrypt(text.getBytes(StandardCharsets.UTF_8));
-            byte[] encryptedText = Arrays.copyOf(salt, salt.length + encrypted.length);
-            System.arraycopy(encrypted, 0, encryptedText, salt.length, encrypted.length);
-            return encryptedText;
+            byte[] encrypted = Encryptors.stronger(password, new String(Hex.encode(salt))).encrypt(message.getBytes(StandardCharsets.UTF_8));
+            byte[] encryptedMessage = Arrays.copyOf(salt, salt.length + encrypted.length);
+            System.arraycopy(encrypted, 0, encryptedMessage, salt.length, encrypted.length);
+            return encryptedMessage;
         } catch (NoSuchAlgorithmException e) {
             throw new IllegalStateException(e);
         }
     }
 
-    public static String decryptText(String password, byte[] encryptedByteArray) {
-        Validate.isTrue(encryptedByteArray.length > PASSWORD_SALT_LENGTH_BYTES);
-        byte[] salt = Arrays.copyOfRange(encryptedByteArray, 0, PASSWORD_SALT_LENGTH_BYTES);
-        byte[] encrypted = Arrays.copyOfRange(encryptedByteArray, PASSWORD_SALT_LENGTH_BYTES, encryptedByteArray.length);
+    public static String decryptMessageByPassword(String password, byte[] encryptedMessage) {
+        Validate.isTrue(encryptedMessage.length > PASSWORD_SALT_LENGTH_BYTES);
+        byte[] salt = Arrays.copyOfRange(encryptedMessage, 0, PASSWORD_SALT_LENGTH_BYTES);
+        byte[] encrypted = Arrays.copyOfRange(encryptedMessage, PASSWORD_SALT_LENGTH_BYTES, encryptedMessage.length);
         byte[] decrypted = Encryptors.stronger(password, new String(Hex.encode(salt))).decrypt(encrypted);
         return new String(decrypted, StandardCharsets.UTF_8);
+    }
+
+    public static PrivateKey createPrivateKey(byte[] pkcs8EncodedKey) {
+        try {
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pkcs8EncodedKey);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return keyFactory.generatePrivate(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static PublicKey createPublicKey(byte[] x509EncodedKey) {
+        try {
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(x509EncodedKey);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return keyFactory.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    /**
+     * @param message Max 190 bytes.
+     */
+    public static byte[] encryptMessageByKey(PublicKey publicKey, String message) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            return cipher.doFinal(message.getBytes());
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static String decryptMessageByKey(PrivateKey privateKey, byte[] encryptedMessage) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] decrypted = cipher.doFinal(encryptedMessage);
+            return new String(decrypted, StandardCharsets.UTF_8);
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static byte[] signMessageByKey(PrivateKey privateKey, String message) {
+        try {
+            Signature signature = Signature.getInstance("SHA256withRSA");
+            signature.initSign(privateKey);
+            signature.update(message.getBytes());
+            return signature.sign();
+        } catch (InvalidKeyException | NoSuchAlgorithmException | SignatureException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static boolean verifySignatureByKey(PublicKey publicKey, String message, byte[] signedMessage) {
+        try {
+            Signature signature = Signature.getInstance("SHA256withRSA");
+            signature.initVerify(publicKey);
+            signature.update(message.getBytes());
+            return signature.verify(signedMessage);
+        } catch (InvalidKeyException | NoSuchAlgorithmException | SignatureException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
--- core/src/test/java/cz/quantumleap/core/common/SecurityUtilsTest.java
+++ core/src/test/java/cz/quantumleap/core/common/SecurityUtilsTest.java
@@ -1,27 +1,71 @@
 package cz.quantumleap.core.common;
 
+import com.google.common.io.ByteStreams;
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SecurityUtilsTest {
 
+    private final PathMatchingResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
+
     @Test
     public void checkEncodedPassword() {
         String password = "asd";
+
         String encodedPassword = SecurityUtils.encodePassword(password);
 
-        assertTrue(SecurityUtils.checkEncodedPassword(password, encodedPassword));
+        assertTrue(SecurityUtils.verifyEncodedPassword(password, encodedPassword));
     }
 
     @Test
-    public void decryptText() {
+    public void decryptMessage() {
         String password = "asd";
-        String text = "asd";
-        byte[] encryptedText = SecurityUtils.encryptText(password, text);
-        String decryptedText = SecurityUtils.decryptText(password, encryptedText);
+        String message = "asd";
 
-        assertEquals(text, decryptedText);
+        byte[] encrypted = SecurityUtils.encryptMessageByPassword(password, message);
+        String decrypted = SecurityUtils.decryptMessageByPassword(password, encrypted);
+
+        assertEquals(message, decrypted);
+    }
+
+    @Test
+    public void decryptTextByKey() {
+        PrivateKey privateKey = SecurityUtils.createPrivateKey(loadResource("classpath:/security/private.der"));
+        PublicKey publicKey = SecurityUtils.createPublicKey(loadResource("classpath:/security/public.der"));
+        String message = "asd";
+
+        byte[] encrypted = SecurityUtils.encryptMessageByKey(publicKey, message);
+        String decrypted = SecurityUtils.decryptMessageByKey(privateKey, encrypted);
+
+        Assert.assertEquals(message, decrypted);
+    }
+
+    @Test
+    public void verifySignatureByKey() {
+        PrivateKey privateKey = SecurityUtils.createPrivateKey(loadResource("classpath:/security/private.der"));
+        PublicKey publicKey = SecurityUtils.createPublicKey(loadResource("classpath:/security/public.der"));
+        String message = "asd";
+
+        byte[] signature = SecurityUtils.signMessageByKey(privateKey, message);
+
+        Assert.assertTrue(SecurityUtils.verifySignatureByKey(publicKey, message, signature));
+    }
+
+    private byte[] loadResource(String location) {
+        try {
+            InputStream inputStream = resourceResolver.getResource(location).getInputStream();
+            return ByteStreams.toByteArray(inputStream);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
