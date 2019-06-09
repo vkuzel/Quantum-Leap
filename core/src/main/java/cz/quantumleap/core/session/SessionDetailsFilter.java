e970554998a35c106fdd44af6e76574fb21a82fb
vkuzel <vkuzel@gmail.com>
2019-06-09 21:11:11 +0200

    * Get remote address from HTTP request method made public.

--- core/src/main/java/cz/quantumleap/core/session/SessionDetailsFilter.java
+++ core/src/main/java/cz/quantumleap/core/session/SessionDetailsFilter.java
@@ -27,7 +27,7 @@ public class SessionDetailsFilter extends OncePerRequestFilter {
         }
     }
 
-    private String getRemoteAddr(HttpServletRequest request) {
+    public static String getRemoteAddr(HttpServletRequest request) {
         // In future there should be a support for RFC 7239 Forwarded header.
         // Unfortunately at the moment nginx does not have built-in support
         // fot the new header so legacy X-Forwarded-For is used.
