8cc45c0ebfaae0b60223449b4e2690deea623f83
vkuzel <vkuzel@gmail.com>
2020-06-28 16:21:13 +0200

    * Upgraded Bootstrap to 4.5.0 * Upgraded jQuery to 3.5.1

--- admin/build.gradle.kts
+++ admin/build.gradle.kts
@@ -1,11 +1,11 @@
 dependencies {
     implementation(project(":core"))
 
-    implementation("org.webjars:bootstrap:4.3.1") {
+    implementation("org.webjars:bootstrap:4.5.0") {
         exclude(group = "org.webjars", module = "jquery")
     }
     implementation("org.webjars:font-awesome:5.8.1")
-    implementation("org.webjars:jquery:3.3.1-2")
+    implementation("org.webjars:jquery:3.5.1")
     implementation("org.webjars.bower:chart.js:2.7.3")
 
     testImplementation(project(":coretest"))
