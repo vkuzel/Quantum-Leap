6743fa61c49a9563b57f888e65f25b3550e696f4
vkuzel <vkuzel@gmail.com>
2020-12-19 15:50:03 +0100

    * Code polishing.

--- core/src/main/resources/db/scripts/00_schema.sql
+++ core/src/main/resources/db/scripts/00_schema.sql
@@ -48,14 +48,14 @@ CREATE TABLE core.enum_value (
 );
 
 CREATE TABLE core.notification (
-                                  id                BIGSERIAL PRIMARY KEY,
-                                  code              VARCHAR    NOT NULL,
-                                  message_arguments VARCHAR [] NOT NULL,
-                                  person_id         BIGINT REFERENCES core.person,
-                                  role_id           BIGINT REFERENCES core.role,
-                                  created_at        TIMESTAMP  NOT NULL,
-                                  resolved_at       TIMESTAMP,
-                                  resolved_by       BIGINT REFERENCES core.person
+  id                BIGSERIAL PRIMARY KEY,
+  code              VARCHAR   NOT NULL,
+  message_arguments VARCHAR[] NOT NULL,
+  person_id         BIGINT REFERENCES core.person,
+  role_id           BIGINT REFERENCES core.role,
+  created_at        TIMESTAMP NOT NULL,
+  resolved_at       TIMESTAMP,
+  resolved_by       BIGINT REFERENCES core.person
 );
 
 CREATE INDEX index_unresolved_notifications
