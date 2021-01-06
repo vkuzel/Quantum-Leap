8516c699dd337aaeefacb242eaa3c709a65119c6
vkuzel <vkuzel@gmail.com>
2021-01-06 11:08:25 +0100

    * Make use of JUnit 5 parametrized tests.

--- core/src/test/java/cz/quantumleap/core/web/template/ResizableImageTagProcessorTest.java
+++ core/src/test/java/cz/quantumleap/core/web/template/ResizableImageTagProcessorTest.java
@@ -3,84 +3,67 @@ package cz.quantumleap.core.web.template;
 import cz.quantumleap.core.web.template.ResizableImageTagProcessor.ResizeCrop;
 import cz.quantumleap.core.web.template.ResizableImageTagProcessor.ResizeLimit;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class ResizableImageTagProcessorTest {
 
-    @Test
-    public void imageIsResized() {
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForLimitTest")
+    public void limitDimensionsAreCorrect(int originalWidth, int originalHeight, int widht, int height,
+                                          int expectedWidth, int expectedHeight) {
         // When
-        ResizeLimit resizeLimit = new ResizeLimit(100, 50, 50, 50);
+        ResizeLimit resizeLimit = new ResizeLimit(originalWidth, originalHeight, widht, height);
 
         // Then
-        Assert.assertEquals(50, resizeLimit.getCalculatedWidth());
-        Assert.assertEquals(25, resizeLimit.getCalculatedHeight());
-
-        // When
-        resizeLimit = new ResizeLimit(50, 100, 50, 50);
-
-        // Then
-        Assert.assertEquals(25, resizeLimit.getCalculatedWidth());
-        Assert.assertEquals(50, resizeLimit.getCalculatedHeight());
-
-        // When
-        resizeLimit = new ResizeLimit(10, 5, 50, 50);
-
-        // Then
-        Assert.assertEquals(50, resizeLimit.getCalculatedWidth());
-        Assert.assertEquals(25, resizeLimit.getCalculatedHeight());
-
-        // When
-        resizeLimit = new ResizeLimit(5, 10, 50, 50);
-
-        // Then
-        Assert.assertEquals(25, resizeLimit.getCalculatedWidth());
-        Assert.assertEquals(50, resizeLimit.getCalculatedHeight());
-
-        // When
-        resizeLimit = new ResizeLimit(100, 10, 50, 50);
-
-        // Then
-        Assert.assertEquals(50, resizeLimit.getCalculatedWidth());
-        Assert.assertEquals(5, resizeLimit.getCalculatedHeight());
+        Assert.assertEquals(expectedWidth, resizeLimit.getCalculatedWidth());
+        Assert.assertEquals(expectedHeight, resizeLimit.getCalculatedHeight());
     }
 
-    @Test
-    public void imageIsCropped() {
+    private static Stream<Arguments> provideArgumentsForLimitTest() {
+        return Stream.of(
+                createLimitTestArguments(100, 50, 50, 50, 50, 25),
+                createLimitTestArguments(50, 100, 50, 50, 25, 50),
+                createLimitTestArguments(10, 5, 50, 50, 50, 25),
+                createLimitTestArguments(5, 10, 50, 50, 25, 50),
+                createLimitTestArguments(100, 10, 50, 50, 50, 5)
+        );
+    }
+
+    private static Arguments createLimitTestArguments(int originalWidth, int originalHeight, int widht, int height,
+                                                      int expectedWidth, int expectedHeight) {
+        return Arguments.of(originalWidth, originalHeight, widht, height, expectedWidth, expectedHeight);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForCropTest")
+    public void cropDimensionsAreCorrect(int originalWidth, int originalHeight, int width, int height,
+                                         int expectedX, int expectedY, int expectedWidth, int expectedHeight) {
         // When
-        ResizeCrop resizeCrop = new ResizeCrop(100, 50, 50, 50);
+        ResizeCrop resizeCrop = new ResizeCrop(originalWidth,originalHeight, width, height);
 
         // Then
-        Assert.assertEquals(25, resizeCrop.getX());
-        Assert.assertEquals(0, resizeCrop.getY());
-        Assert.assertEquals(50, resizeCrop.getCropWidth());
-        Assert.assertEquals(50, resizeCrop.getCropHeight());
+        Assert.assertEquals(expectedX, resizeCrop.getX());
+        Assert.assertEquals(expectedY, resizeCrop.getY());
+        Assert.assertEquals(expectedWidth, resizeCrop.getCropWidth());
+        Assert.assertEquals(expectedHeight, resizeCrop.getCropHeight());
+    }
 
-        // When
-        resizeCrop = new ResizeCrop(50, 100, 50, 50);
+    private static Stream<Arguments> provideArgumentsForCropTest() {
+        return Stream.of(
+                createCropTestArguments(100, 50, 50, 50, 25, 0, 50, 50),
+                createCropTestArguments(50, 100, 50, 50, 0, 25, 50, 50),
+                createCropTestArguments(10, 5, 50, 50, 2, 0, 5, 5),
+                createCropTestArguments(5, 10, 50, 50, 0, 2, 5, 5)
+        );
+    }
 
-        // Then
-        Assert.assertEquals(0, resizeCrop.getX());
-        Assert.assertEquals(25, resizeCrop.getY());
-        Assert.assertEquals(50, resizeCrop.getCropWidth());
-        Assert.assertEquals(50, resizeCrop.getCropHeight());
-
-        // When
-        resizeCrop = new ResizeCrop(10, 5, 50, 50);
-
-        // Then
-        Assert.assertEquals(2, resizeCrop.getX());
-        Assert.assertEquals(0, resizeCrop.getY());
-        Assert.assertEquals(5, resizeCrop.getCropWidth());
-        Assert.assertEquals(5, resizeCrop.getCropHeight());
-
-        // When
-        resizeCrop = new ResizeCrop(5, 10, 50, 50);
-
-        // Then
-        Assert.assertEquals(0, resizeCrop.getX());
-        Assert.assertEquals(2, resizeCrop.getY());
-        Assert.assertEquals(5, resizeCrop.getCropWidth());
-        Assert.assertEquals(5, resizeCrop.getCropHeight());
+    private static Arguments createCropTestArguments(int originalWidth, int originalHeight, int width, int height,
+                                                     int expectedX, int expectedY, int expectedWidth, int expectedHeight) {
+        return Arguments.of(originalWidth, originalHeight, width, height,
+                expectedX, expectedY, expectedWidth, expectedHeight);
     }
 }
