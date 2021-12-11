4570836ab03558f28293138ecf1491778333b131
vkuzel <vkuzel@gmail.com>
2021-12-11 21:26:49 +0100

    * Use Spring-configured Jackson ObjectMapper in the Thymeleaf instead of Thymeleaf-configured ObjectMapper. Reason: Spring (among other things) recognises custom serializers registered by @JsonComponent. * Copied some Thymeleaf-Jackson private configuration classes to the QuantumLeaps configuration.

--- core/src/main/java/cz/quantumleap/core/view/ThymeleafConfiguration.java
+++ core/src/main/java/cz/quantumleap/core/view/ThymeleafConfiguration.java
@@ -1,5 +1,13 @@
 package cz.quantumleap.core.view;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.SerializableString;
+import com.fasterxml.jackson.core.io.CharacterEscapes;
+import com.fasterxml.jackson.core.io.SerializedString;
+import com.fasterxml.jackson.core.json.JsonWriteFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import cz.quantumleap.core.filestorage.FileStorageManager;
 import cz.quantumleap.core.resource.ResourceManager;
 import cz.quantumleap.core.view.template.QuantumLeapDialect;
@@ -7,37 +15,101 @@ import cz.quantumleap.core.view.template.ThemeTemplateResolver;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.exceptions.TemplateProcessingException;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.dialect.SpringStandardDialect;
+import org.thymeleaf.standard.serializer.IStandardJavaScriptSerializer;
+
+import java.io.IOException;
+import java.io.Writer;
 
 @Configuration
 @ConditionalOnWebApplication
 public class ThymeleafConfiguration {
 
-    private final ThymeleafProperties thymeleafProperties;
-    private final ResourceManager resourceManager;
-    private final MessageSource messageSource;
-    private final FileStorageManager fileStorageManager;
-
-    public ThymeleafConfiguration(ThymeleafProperties thymeleafProperties, ResourceManager resourceManager, MessageSource messageSource, FileStorageManager fileStorageManager) {
-        this.thymeleafProperties = thymeleafProperties;
-        this.resourceManager = resourceManager;
-        this.messageSource = messageSource;
-        this.fileStorageManager = fileStorageManager;
-    }
-
     @Bean
-    public SpringTemplateEngine thymeleafTemplateEngine() {
+    public SpringTemplateEngine thymeleafTemplateEngine(
+            ThymeleafProperties thymeleafProperties,
+            ResourceManager resourceManager,
+            FileStorageManager fileStorageManager,
+            ObjectMapper objectMapper
+    ) {
+        ObjectMapper mapper = objectMapper.copy();
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+        SerializationConfig serializationConfig = mapper.getSerializationConfig()
+                .with(JsonWriteFeature.ESCAPE_NON_ASCII);
+        mapper.setConfig(serializationConfig);
+        mapper.getFactory().setCharacterEscapes(new JacksonThymeleafCharacterEscapes());
+
         SpringTemplateEngine engine = new SpringTemplateEngine();
+        SpringStandardDialect dialect = new SpringStandardDialect();
+        dialect.setJavaScriptSerializer(new JavaScriptSerializer(mapper));
+        engine.setDialect(dialect);
+
         engine.setTemplateResolver(new ThemeTemplateResolver(thymeleafProperties, resourceManager));
         engine.addDialect(new SpringSecurityDialect());
         engine.addDialect(new LayoutDialect());
         engine.addDialect(new Java8TimeDialect());
         engine.addDialect(new QuantumLeapDialect(fileStorageManager));
         return engine;
+    }
+
+    /**
+     * Copied from {@link org.thymeleaf.standard.serializer.StandardJavaScriptSerializer}
+     */
+    private static class JacksonThymeleafCharacterEscapes extends CharacterEscapes {
+
+        private static final int[] CHARACTER_ESCAPES;
+        private static final SerializableString SLASH_ESCAPE = new SerializedString("\\/");
+        private static final SerializableString AMPERSAND_ESCAPE = new SerializedString("\\u0026");
+
+        static {
+            CHARACTER_ESCAPES = CharacterEscapes.standardAsciiEscapesForJSON();
+            CHARACTER_ESCAPES['/'] = CharacterEscapes.ESCAPE_CUSTOM;
+            CHARACTER_ESCAPES['&'] = CharacterEscapes.ESCAPE_CUSTOM;
+        }
+
+        @Override
+        public int[] getEscapeCodesForAscii() {
+            return CHARACTER_ESCAPES;
+        }
+
+        @Override
+        public SerializableString getEscapeSequence(final int ch) {
+            if (ch == '/') {
+                return SLASH_ESCAPE;
+            } else if (ch == '&') {
+                return AMPERSAND_ESCAPE;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    /**
+     * Copied from {@link org.thymeleaf.standard.serializer.StandardJavaScriptSerializer}
+     */
+    private static class JavaScriptSerializer implements IStandardJavaScriptSerializer {
+
+        private final ObjectMapper mapper;
+
+        public JavaScriptSerializer(ObjectMapper mapper) {
+            this.mapper = mapper;
+        }
+
+        @Override
+        public void serializeValue(Object object, Writer writer) {
+            try {
+                mapper.writeValue(writer, object);
+            } catch (IOException e) {
+                String msg = "An exception was raised while trying to serialize object to JavaScript using Jackson";
+                throw new TemplateProcessingException(msg, e);
+            }
+        }
     }
 }
