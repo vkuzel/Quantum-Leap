bf9a8953acb6b21113d7e9c71b08dd06a8ec229f
vkuzel <vkuzel@gmail.com>
2023-08-09 07:21:04 +0200

    Extract multi-map utils into a separate class.

---
+++ core/src/main/java/cz/quantumleap/core/common/MultiMaps.java
@@ -0,0 +1,33 @@
+package cz.quantumleap.core.common;
+
+import java.util.*;
+import java.util.function.Function;
+
+public final class MultiMaps {
+
+    public static <K, V> Map<K, List<V>> groupBy(Collection<V> collection, Function<V, K> groupBy) {
+        Map<K, List<V>> mapOfLists = new HashMap<>();
+        for (V value : collection) {
+            K key = groupBy.apply(value);
+            List<V> values = mapOfLists.computeIfAbsent(key, k -> new ArrayList<>());
+            values.add(value);
+        }
+        return mapOfLists;
+    }
+
+    public static <V> List<V> toValues(Map<?, List<V>> mapOfLists) {
+        List<V> values = new ArrayList<>();
+        mapOfLists.forEach((o, vs) -> values.addAll(vs));
+        return values;
+    }
+
+    public static <V, T> List<T> mapValues(Map<?, List<V>> mapOfLists, Function<V, T> valueMap) {
+        List<T> values = new ArrayList<>();
+        mapOfLists.forEach((o, vs) -> {
+            for (V v : vs) {
+                values.add(valueMap.apply(v));
+            }
+        });
+        return values;
+    }
+}
--- core/src/main/java/cz/quantumleap/core/common/Utils.java
+++ core/src/main/java/cz/quantumleap/core/common/Utils.java
@@ -68,32 +68,6 @@ final public class Utils {
         return max;
     }
 
-    public static <K, V> Map<K, List<V>> groupBy(Collection<V> collection, Function<V, K> groupBy) {
-        Map<K, List<V>> mapOfLists = new HashMap<>();
-        for (V value : collection) {
-            K key = groupBy.apply(value);
-            List<V> values = mapOfLists.computeIfAbsent(key, k -> new ArrayList<>());
-            values.add(value);
-        }
-        return mapOfLists;
-    }
-
-    public static <V> List<V> toValues(Map<?, List<V>> mapOfLists) {
-        List<V> values = new ArrayList<>();
-        mapOfLists.forEach((o, vs) -> values.addAll(vs));
-        return values;
-    }
-
-    public static <V, T> List<T> mapValues(Map<?, List<V>> mapOfLists, Function<V, T> valueMap) {
-        List<T> values = new ArrayList<>();
-        mapOfLists.forEach((o, vs) -> {
-            for (V v : vs) {
-                values.add(valueMap.apply(v));
-            }
-        });
-        return values;
-    }
-
     public static <T> String createAbbreviation(Collection<T> items, int maxSize, Function<T, String> mapToText) {
         Set<String> textItems = new LinkedHashSet<>(maxSize);
         for (T item : items) {
