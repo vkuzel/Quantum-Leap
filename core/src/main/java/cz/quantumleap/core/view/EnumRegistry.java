3f09cfba6bfc509e6ae8c9c8859984b64d092124
vkuzel <vkuzel@gmail.com>
2023-08-10 07:51:40 +0200

    Enums sorted in natural order.

--- core/src/main/java/cz/quantumleap/core/view/EnumRegistry.java
+++ core/src/main/java/cz/quantumleap/core/view/EnumRegistry.java
@@ -1,26 +1,23 @@
 package cz.quantumleap.core.view;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
-import com.google.common.collect.Tables;
 import cz.quantumleap.core.database.domain.IdLabel;
 import org.jooq.DSLContext;
-import org.jooq.Record3;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static cz.quantumleap.core.tables.EnumValueTable.ENUM_VALUE;
+import static java.lang.String.CASE_INSENSITIVE_ORDER;
 
 @Component
 public class EnumRegistry {
 
     private final DSLContext dslContext;
 
-    private volatile Table<String, String, String> enumValueLabels;
+    private volatile Map<String, Map<String, String>> enumValueLabels;
 
     public EnumRegistry(DSLContext dslContext) {
         this.dslContext = dslContext;
@@ -28,22 +25,37 @@ public class EnumRegistry {
 
     @SuppressWarnings("unused")
     public List<IdLabel> getValueLabels(String enumId) {
-        return enumValueLabels.row(enumId).entrySet()
-                .stream()
+        return enumValueLabels.getOrDefault(enumId, Map.of())
+                .entrySet().stream()
                 .map(e -> new IdLabel(e.getKey(), e.getValue()))
                 .collect(Collectors.toList());
     }
 
     @SuppressWarnings("unused")
     public String getLabel(String enumId, String value) {
-        return enumValueLabels.get(enumId, value);
+        return enumValueLabels.getOrDefault(enumId, Map.of()).get(value);
     }
 
     @EventListener(ContextRefreshedEvent.class)
     public void loadEnums() {
-        enumValueLabels = dslContext.select(ENUM_VALUE.ENUM_ID, ENUM_VALUE.ID, ENUM_VALUE.LABEL)
+        enumValueLabels = new HashMap<>();
+        dslContext
+                .select(ENUM_VALUE.ENUM_ID, ENUM_VALUE.ID, ENUM_VALUE.LABEL)
                 .from(ENUM_VALUE)
                 .fetchStream()
-                .collect(Tables.toTable(Record3::value1, Record3::value2, Record3::value3, HashBasedTable::create));
+                .map(record -> new EnumIdLabel(record.value1(), record.value2(), record.value3()))
+                .sorted(Comparator.comparing(EnumIdLabel::label, CASE_INSENSITIVE_ORDER))
+                .forEach(enumIdLabel -> {
+                    Map<String, String> idLabel = enumValueLabels
+                            .computeIfAbsent(enumIdLabel.enumId, s -> new LinkedHashMap<>());
+                    idLabel.put(enumIdLabel.id(), enumIdLabel.label());
+                });
+    }
+
+    private record EnumIdLabel(
+            String enumId,
+            String id,
+            String label
+    ) {
     }
 }
