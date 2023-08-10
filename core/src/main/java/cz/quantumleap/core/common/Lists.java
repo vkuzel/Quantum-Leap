f14fb654770fd2c626435c0d5895452fc52742a7
vkuzel <vkuzel@gmail.com>
2023-08-10 21:07:22 +0200

    Mark array list factory w/ @SafeVarargs.

--- core/src/main/java/cz/quantumleap/core/common/Lists.java
+++ core/src/main/java/cz/quantumleap/core/common/Lists.java
@@ -7,6 +7,7 @@ import static java.util.Arrays.asList;
 
 public final class Lists {
 
+    @SafeVarargs
     public static <T> List<T> newArrayList(T... items) {
         return new ArrayList<>(asList(items));
     }
